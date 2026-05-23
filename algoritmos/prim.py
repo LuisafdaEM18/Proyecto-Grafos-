6ad5673100b83f8017ae177bab3933aedf11d8e3
LuisafdaEM18 <lespinalmontoya@gmail.com>
2026-05-22 20:28:54 -0500

    Implementar clase Vertice

---
+++ algoritmos/prim.py
@@ -0,0 +1,2 @@
+def prim(grafo):
+    pass
--- modelos/vertice.py
+++ modelos/vertice.py
@@ -1,2 +1,21 @@
 class Vertice:
-    pass
+   
+    def __init__(self, id: str, nombre: str, tipo: str, descripcion: str = ""):
+        self.id = id
+        self.nombre = nombre
+        self.tipo = tipo
+        self.descripcion = descripcion
+
+    def __repr__(self):
+        return f"Vertice({self.id}: {self.nombre})"
+
+    def __str__(self):
+        return f"[{self.id}] {self.nombre} ({self.tipo})"
+
+    def __eq__(self, otro):
+        if isinstance(otro, Vertice):
+            return self.id == otro.id
+        return False
+
+    def __hash__(self):
+        return hash(self.id)
